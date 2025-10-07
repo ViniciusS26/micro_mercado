32bc1bc8398bd0361c512bd7d9758874ffb391c8
Vinicius Nunes <viniciusdasilvanunes773@gmail.com>
2025-10-07 20:39:20 -0300

    acessando url do banco de dados local

---
+++ services/ms-produtos/app/db/database.py
@@ -0,0 +1,18 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:root@localhost:5432/db_produtos" # url bd local
+    #"postgresql://postgres:vini123@db_funcionarios/funcionarios_db" url de produção 
+)
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+
+Base = declarative_base()
