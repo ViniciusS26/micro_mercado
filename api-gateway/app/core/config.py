9098b488638d84e68cc10b75a710db84d4696478
Vinicius Nunes <viniciusdasilvanunes773@gmail.com>
2025-11-06 09:06:27 -0300

    fix alterando porta

--- api-gateway/app/core/config.py
+++ api-gateway/app/core/config.py
@@ -1,7 +1,7 @@
 import os
 
 class Settings:
-    MS_FUNCIONARIOS_URL: str = os.getenv("MS_FUNCIONARIOS_URL", "http://54.235.39.176/api/v1/funcionarios/")
+    MS_FUNCIONARIOS_URL: str = os.getenv("MS_FUNCIONARIOS_URL", "http://54.235.39.176/api/v1/funcionarios")
     MS_PRODUTOS_URL: str = os.getenv("MS_PRODUTOS_URL", "http://34.234.88.237/api/v1/produtos/")
     MS_VENDAS_URL: str = os.getenv("MS_VENDAS_URL", "http://3.91.44.244/api/v1/vendas")
     MS_RELATORIOS_URL: str = os.getenv("MS_RELATORIOS_URL", "http:/52.91.185.99/api/v1/relatorios")
