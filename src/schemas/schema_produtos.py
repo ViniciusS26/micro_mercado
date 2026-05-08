c951b187f623a7a7d6d7ec565b3eddb71258af51
Vinicius Nunes <viniciusdasilvanunes773@gmail.com>
2026-05-08 01:02:19 -0300

    refact: refatorando projeto para solução mais simples

---
+++ src/schemas/schema_produtos.py
@@ -0,0 +1,38 @@
+from pydantic import BaseModel,field_validator, ConfigDict
+from typing import List, Optional
+from datetime import date, datetime
+from validate_docbr import CPF
+
+class ProdutoBase(BaseModel):
+    id: Optional[int] = None
+    titulo: str
+    descricao: str
+    preco: float
+    peso: float
+    data_fabricacao: date
+    data_validade: date
+    data_cadastro: Optional[datetime] = None
+    data_atualizacao: Optional[datetime] = None
+
+class ProdutoCreate(BaseModel):
+    titulo: str
+    descricao: str
+    preco: float
+    peso: float
+    data_fabricacao: date
+    data_validade: date
+
+   
+
+
+class ProdutoUpdate(BaseModel):
+    titulo: Optional[str] = None
+    descricao: Optional[str] = None 
+    preco: Optional[float] = None
+    peso: Optional[float] = None
+
+class Produto(ProdutoBase):
+    id: int
+
+    class Config:
+        orm_mode = True
