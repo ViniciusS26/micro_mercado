655f9841fb92d1c8dc5157588270cace1dca67df
Vinicius Nunes <viniciusdasilvanunes773@gmail.com>
2026-05-20 00:26:01 -0300

    feat: adicionando middlwares de acesso a hosts

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from routes import  routes_funcionario, routes_produtos, routes_vendas, routes_relatorio
 from db.connection import engine, Base
 
@@ -13,10 +14,18 @@ app = FastAPI(
     version="1.0.0"
 )
 
+origins = [
+    #"http://localhost",
+    #"http://localhost:3000", """
+    "https://sistema.viniciuss.com.br"
+]
+
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    TrustedHostMiddleware,
+    allowed_hosts=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"], 
